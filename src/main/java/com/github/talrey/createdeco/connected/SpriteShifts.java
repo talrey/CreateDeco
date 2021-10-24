bd72d2923ed06c4c0610f7e13f696504b71d3dfa
cofl <cllacour@mtu.edu>
2021-10-24 12:17:11 -0400

    Use Create.asResource(String) method consistent with Create source

--- src/main/java/com/github/talrey/createdeco/connected/SpriteShifts.java
+++ src/main/java/com/github/talrey/createdeco/connected/SpriteShifts.java
@@ -22,12 +22,12 @@ public class SpriteShifts {
     for (String metal : Registration.METAL_TYPES.keySet()) {
       String path = "block/palettes/sheet_metal/" + metal.toLowerCase() + "_sheet_metal";
       ResourceLocation blockTexture = new ResourceLocation(CreateDecoMod.MODID, path);
-      ResourceLocation connectedTexture = new ResourceLocation(Create.ID, path + "_connected");
+      ResourceLocation connectedTexture = Create.asResource(path + "_connected");
       SHEET_METAL_SIDES.put(metal, CTSpriteShifter.getCT(CTType.VERTICAL, blockTexture, connectedTexture));
 
       path = "block/palettes/catwalks/" + metal.toLowerCase() + "_catwalk";
       blockTexture = new ResourceLocation(CreateDecoMod.MODID, path);
-      connectedTexture = new ResourceLocation(Create.ID, path + "_connected");
+      connectedTexture = Create.asResource(path + "_connected");
       CATWALK_TOPS.put(metal, CTSpriteShifter.getCT(CTType.OMNIDIRECTIONAL, blockTexture, connectedTexture));
     }
   }
