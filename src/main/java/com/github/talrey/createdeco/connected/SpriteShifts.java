caab90513aa965a080b1ec9511c0daccf0a67035
Talrey <cycleman.atl@gmail.com>
2021-10-24 18:18:14 -0700

    fixed compat with Create 0.3.2f

--- src/main/java/com/github/talrey/createdeco/connected/SpriteShifts.java
+++ src/main/java/com/github/talrey/createdeco/connected/SpriteShifts.java
@@ -21,13 +21,13 @@ public class SpriteShifts {
   private static void populateMaps () {
     for (String metal : Registration.METAL_TYPES.keySet()) {
       String path = "block/palettes/sheet_metal/" + metal.toLowerCase() + "_sheet_metal";
-      ResourceLocation blockTexture = new ResourceLocation(CreateDecoMod.MODID, path);
-      ResourceLocation connectedTexture = Create.asResource(path + "_connected");
+      ResourceLocation blockTexture     = new ResourceLocation(CreateDecoMod.MODID, path);
+      ResourceLocation connectedTexture = new ResourceLocation(CreateDecoMod.MODID, path + "_connected");
       SHEET_METAL_SIDES.put(metal, CTSpriteShifter.getCT(CTType.VERTICAL, blockTexture, connectedTexture));
 
       path = "block/palettes/catwalks/" + metal.toLowerCase() + "_catwalk";
-      blockTexture = new ResourceLocation(CreateDecoMod.MODID, path);
-      connectedTexture = Create.asResource(path + "_connected");
+      blockTexture     = new ResourceLocation(CreateDecoMod.MODID, path);
+      connectedTexture = new ResourceLocation(CreateDecoMod.MODID, path + "_connected");
       CATWALK_TOPS.put(metal, CTSpriteShifter.getCT(CTType.OMNIDIRECTIONAL, blockTexture, connectedTexture));
     }
   }
