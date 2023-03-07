dba6d2b1e38961fba7389f3d9665c1e06c8685f8
Talrey <cycleman.atl@gmail.com>
2023-03-07 15:04:19 -0800

    light adjustments

--- src/main/java/com/github/talrey/createdeco/blocks/SupportBlock.java
+++ src/main/java/com/github/talrey/createdeco/blocks/SupportBlock.java
@@ -1,15 +1,25 @@
 package com.github.talrey.createdeco.blocks;
 
 import com.simibubi.create.foundation.utility.Iterate;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.DirectionalBlock;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
-public class SupportBlock extends DirectionalBlock {
+public class SupportBlock extends DirectionalBlock implements SimpleWaterloggedBlock {
   public SupportBlock (Properties props) {
     super(props);
   }
@@ -17,22 +27,31 @@ public class SupportBlock extends DirectionalBlock {
   @Override
   protected void createBlockStateDefinition (StateDefinition.Builder<Block, BlockState> builder) {
     builder.add(FACING);
+    builder.add(BlockStateProperties.WATERLOGGED);
   }
 
   @Nullable
   @Override
   public BlockState getStateForPlacement (BlockPlaceContext ctx) {
-    BlockState result = super.getStateForPlacement(ctx).setValue(FACING,
-      ctx.getClickedFace()
-    );
-    for (Direction side : Iterate.directions) {
-      BlockState neighbor = ctx.getLevel()
-        .getBlockState(ctx.getClickedPos()
-          .relative(side));
-      if (neighbor.getBlock() instanceof SupportBlock) {
-        result = result.setValue(FACING, neighbor.getValue(FACING));
-      }
-    }
+    FluidState fluid = ctx.getLevel().getFluidState(ctx.getClickedPos());
+    BlockState result = defaultBlockState()
+      .setValue(FACING,ctx.getClickedFace())
+      .setValue(BlockStateProperties.WATERLOGGED, fluid.is(Fluids.WATER));
     return result;
+  }
+
+  @Override
+  public boolean canPlaceLiquid (BlockGetter world, BlockPos pos, BlockState state, Fluid fluid) {
+    return !state.getValue(BlockStateProperties.WATERLOGGED) && fluid == Fluids.WATER;
+  }
+
+  @Override
+  public FluidState getFluidState(BlockState state) {
+    return state.getValue(BlockStateProperties.WATERLOGGED) ? Fluids.WATER.getSource(false) : Fluids.EMPTY.defaultFluidState();
+  }
+
+  @Override
+  public VoxelShape getVisualShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext ctx) {
+    return Shapes.empty();
   }
 }
