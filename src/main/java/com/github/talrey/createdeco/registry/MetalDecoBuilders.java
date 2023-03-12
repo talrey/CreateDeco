9991af0990a78ed6c9c976400c07b8af384ed287
Talrey <cycleman.atl@gmail.com>
2023-03-12 12:58:35 -0700

    fast-forward clean up

--- src/main/java/com/github/talrey/createdeco/registry/MetalDecoBuilders.java
+++ src/main/java/com/github/talrey/createdeco/registry/MetalDecoBuilders.java
@@ -32,6 +32,7 @@ import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePrope
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.minecraftforge.client.model.generators.BlockModelBuilder;
 import net.minecraftforge.client.model.generators.MultiPartBlockStateBuilder;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Locale;
 import java.util.function.Function;
