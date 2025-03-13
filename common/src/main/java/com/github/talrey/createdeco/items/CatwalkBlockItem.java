4c474637dab7a9e77744ec5b49f6e39726ea0e41
snailcase <203162583+snailcase@users.noreply.github.com>
2025-03-13 21:41:08 +0100

    Replace railing block with catwalk block seamlessly
    When placing a catwalk block into an existing railing block, the railing block is replaced
    by a catwalk block with the appropriate block state set.

--- common/src/main/java/com/github/talrey/createdeco/items/CatwalkBlockItem.java
+++ common/src/main/java/com/github/talrey/createdeco/items/CatwalkBlockItem.java
@@ -1,12 +1,15 @@
 package com.github.talrey.createdeco.items;
 
+import com.github.talrey.createdeco.BlockRegistry;
 import com.github.talrey.createdeco.blocks.CatwalkBlock;
+import com.github.talrey.createdeco.blocks.CatwalkRailingBlock;
 import com.simibubi.create.foundation.placement.IPlacementHelper;
 import com.simibubi.create.foundation.placement.PlacementHelpers;
 import com.simibubi.create.foundation.placement.PlacementOffset;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
@@ -22,11 +25,13 @@ import java.util.function.Predicate;
 public class CatwalkBlockItem extends BlockItem {
   private final int extensionPlacementHelperID;
   private final int catwalkPlacementHelperID;
+  private final int catwalkReplacementHelperID;
 
   public CatwalkBlockItem (CatwalkBlock block, Properties props) {
     super(block, props);
     extensionPlacementHelperID = PlacementHelpers.register(new CatwalkExtensionHelper());
     catwalkPlacementHelperID = PlacementHelpers.register(new CatwalkPlacementHelper());
+    catwalkReplacementHelperID = PlacementHelpers.register(new CatwalkReplacementHelper());
   }
 
   @Override
@@ -39,6 +44,7 @@ public class CatwalkBlockItem extends BlockItem {
     BlockState state        = world.getBlockState(pos);
     IPlacementHelper extension_helper = PlacementHelpers.get(extensionPlacementHelperID);
     IPlacementHelper placement_helper = PlacementHelpers.get(catwalkPlacementHelperID);
+    IPlacementHelper replacement_helper = PlacementHelpers.get(catwalkReplacementHelperID);
     BlockHitResult ray = new BlockHitResult(ctx.getClickLocation(), face, pos, true);
     if (extension_helper.matchesState(state) && player != null) {
       return extension_helper.getOffset(player, world, state, pos, ray).placeInWorld(world, this, player, ctx.getHand(), ray);
@@ -52,10 +58,20 @@ public class CatwalkBlockItem extends BlockItem {
       world.setBlock(pos, state, 3);
       ItemStack stack = ctx.getItemInHand();
       if (!player.getAbilities().instabuild) {
-	stack.shrink(1);
+        stack.shrink(1);
       }
 
       return InteractionResult.SUCCESS;
+    } else if(replacement_helper.matchesState(state) && player != null) {
+      PlacementOffset offset = replacement_helper.getOffset(player, world, state, pos, ray);
+      if (offset.isSuccessful()) {
+        // The placeInWorld function has code to first check whether the current
+        // block can be replaced. That's not the case for catwalk railings, so
+        // we briefly replace the block with air, and then replace it with the
+        // catwalk block.
+        world.setBlock(pos, Blocks.AIR.defaultBlockState(), 0);
+        return offset.placeInWorld(world, this, player, ctx.getHand(), ray);
+      }
     }
     return super.useOn(ctx);
   }
@@ -145,6 +161,49 @@ public class CatwalkBlockItem extends BlockItem {
 	.setValue(CatwalkBlock.RAILING_WEST, false);
 
       return PlacementOffset.success(pos, offsetState -> ghost_state);
+    }
+  }
+
+  /**
+     This helper is used to replace railing blocks with catwalk blocks when the player interacts with a legacy railing block.
+     This allows seamlessly replacing legacy blocks with catwalk blocks, without needing to breaking and replacing the block.
+   **/
+  @MethodsReturnNonnullByDefault
+  public static class CatwalkReplacementHelper implements IPlacementHelper {
+    @Override
+    public Predicate<ItemStack> getItemPredicate () {
+      return CatwalkBlock::isCatwalk;
+    }
+
+    @Override
+    public Predicate<BlockState> getStatePredicate () {
+      return state -> CatwalkRailingBlock.isRailing(state.getBlock());
+    }
+
+    @Override
+    public PlacementOffset getOffset(Player player, Level world, BlockState state, BlockPos pos, BlockHitResult ray) {
+      // The ghost state should only contain the bottom catwalk
+
+      if (state.getBlock() instanceof CatwalkRailingBlock catrail) {
+	String metal = catrail.metal;
+	// The ghost state only shows the added catwal
+	BlockState ghost_state = BlockRegistry.CATWALKS.get(metal).get()
+	  .defaultBlockState()
+	  .setValue(CatwalkBlock.CATWALK_BOTTOM, true);
+
+	// But the updated state contains the new catwalk as well as any existing railings
+	BlockState new_state = BlockRegistry.CATWALKS.get(metal).get()
+	  .defaultBlockState()
+	  .setValue(CatwalkBlock.CATWALK_TOP, false)
+	  .setValue(CatwalkBlock.CATWALK_BOTTOM, true)
+	  .setValue(CatwalkBlock.RAILING_NORTH, state.getValue(CatwalkRailingBlock.NORTH_FENCE))
+	  .setValue(CatwalkBlock.RAILING_SOUTH, state.getValue(CatwalkRailingBlock.SOUTH_FENCE))
+	  .setValue(CatwalkBlock.RAILING_EAST, state.getValue(CatwalkRailingBlock.EAST_FENCE))
+	  .setValue(CatwalkBlock.RAILING_WEST, state.getValue(CatwalkRailingBlock.WEST_FENCE));
+
+        return PlacementOffset.success(pos, offsetState -> new_state).withGhostState(ghost_state);
+      }
+      return PlacementOffset.fail();
     }
   }
 }
